8963c863ebe0bc25180c3d60817d4453208538e2
talent-dev610 <107520936+talent-dev610@users.noreply.github.com>
2022-07-29 22:11:04 +0300

    commit 2

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require("express");
 var app = express();
 app.use(express.json());
 app.use("/testmode",(req, res) =>{
-   console.log(req.body);
+   var data = req.body;
+   console.log(data);
    res.json({
-    req+"hello"
-});
+    "index1":"1",
+    "index2":"2"
+   }
+   )
 })
 
 app.get("/apple-app-site-association",function(req,res){
